f76a2629f6ae5c3453d543999fc83b6d438921f3
hathaipat-p <hathaipat44@hotmail.co.th>
2022-04-29 23:55:34 +0700

    set current time in setup()
    add read datetime from RTC

--- chatbot-arduino.ino
+++ chatbot-arduino.ino
@@ -3,6 +3,8 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <TridentTD_LineNotify.h>
+#include <WiFi.h>
+#include "time.h"
 
 unsigned long period = 10000; //ระยะเวลาที่ต้องการรอ TempCheck
 unsigned long last_time = 0; // ใช้สำหรับเทียบเวลาเพื่อ TempCheck
@@ -16,8 +18,12 @@ unsigned long last_time = 0; // ใช้สำหรับเทียบเว
 
 #define LINE_TOKEN  "aiMrxdEESK9zqof7STDcDcqbsLI7KSWBs92q9hqJ6Ik"   // token line notify
 
-const char* ssid = "true_home2G_110";
-const char* password = "bpsadgk4";
+const char* ssid = "POM";
+const char* password = "0800778528";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 25200;
 
 // Assign output variables to GPIO pins
 const int output26 = 26;
@@ -148,8 +154,88 @@ void checkTemp(){
   }
 }
 
+// Set Current time //
+byte DecToBcd(byte num){
+ return ( (num/10*16) + (num%10) );
+}
+
+byte BcdToDec(byte bin){
+ return ( (bin/16*10) + (bin%16) );
+}
+
+void setDateTime(byte sec, byte mint,byte hour, byte wday, byte date, byte month, byte year){
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write(RTC_LOCATION);
+  Wire.write(DecToBcd(sec));
+  Wire.write(DecToBcd(mint));
+  Wire.write(DecToBcd(hour));
+  Wire.write(DecToBcd(wday));
+  Wire.write(DecToBcd(date));
+  Wire.write(DecToBcd(month));
+  Wire.write(DecToBcd(year));
+  Wire.endTransmission();
+
+  // start clock
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write(RTC_LOCATION);
+  Wire.write(DecToBcd(sec)| 0b10000000);
+  Wire.endTransmission();
+}
+
+String getDate(){
+  byte date, month, year;
+  String DD, MM, YY, Date;
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write(0x04);
+  Wire.endTransmission();
+
+  Wire.requestFrom(RTC_ADDR, 3);
+  date       = BcdToDec(Wire.read() & 0x3f);  // 0b00111111
+  month      = BcdToDec(Wire.read() & 0x1f);  // 0b00011111
+  year       = BcdToDec(Wire.read());         
+
+  //  format date into DD/MM/YY
+  DD = String(date, DEC);
+  MM = String(month, DEC);
+  YY = String(year, DEC);
+  Date += DD + "/" + MM + "/" + YY;
+  return Date;
+}
+
+String getTime(){
+  byte second, minute, hour;
+  String Time, HH, MN, SS;
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  Wire.requestFrom(RTC_ADDR, 3);
+  second     = BcdToDec(Wire.read() & 0x7f);  // 0b01111111
+  minute     = BcdToDec(Wire.read() & 0x7f);  // 0b01111111
+  hour       = BcdToDec(Wire.read() & 0x3f);  // 0b01111111       
+
+//  format time into HH:MN:SS
+  HH = String(hour, DEC);
+  
+  if (minute < 10) {
+    MN = "0";
+  }
+  MN += String(minute, DEC);
+
+  if (second < 10) {
+    SS = "0";
+  }
+  SS += String(second, DEC);
+  
+  Time += HH + ":" + MN + ":" + SS;
+  return Time;
+}
+
+
+
 void setup() {     
   Wire.begin(I2C_SDA, I2C_SCL);
+  Wire.begin(4, 5);
   Serial.begin(115200);  
   pinMode(output26, OUTPUT);  // IO26
   pinMode(output27, OUTPUT);  // IO27
@@ -163,16 +249,37 @@ void setup() {
 
   connectToWiFi();
   setup_routing();
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);       // get current time from ntpserver
+
+  int mday, mon, year, sec, min, hour, wday;
+  
+  struct tm timeinfo;
+  while(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+  }
+  
+  mday = timeinfo.tm_mday;
+  mon = timeinfo.tm_mon+1;
+  year = timeinfo.tm_year+1900-2000;
+  sec = timeinfo.tm_sec;
+  min = timeinfo.tm_min;
+  hour = timeinfo.tm_hour;
+  wday = timeinfo.tm_wday+1;
+  
+  setDateTime(sec,min,hour,wday,mday,mon,year);
 }    
        
 void loop() {    
   server.handleClient();
+  
   // เตือนอุณหภูมิสูงทุกๆ 10 วินาที (สำหรับเทส ตอนจริงเปลี่ยนเวลาได้)
   if( millis() - last_time > period) {
      last_time = millis();
      Serial.println("Check Temp");
      checkTemp();
   }
+  
   // buzzer เตือนอุณหภูมิสูงดัง 5 วินาทีแล้วดับ
   if (buzStatus == true) {
     if( millis() - last_time > 5000) {
@@ -181,4 +288,12 @@ void loop() {
      Serial.println("Buzzer Alarm Finish");
     }
   }
+
+  if( millis() - last_time > 1000) {    // get time every sec
+     last_time = millis();
+     Serial.print("Time: ");
+     printf("%s ", getDate());
+     printf("%s\n", getTime());
+  }
+  
 }
